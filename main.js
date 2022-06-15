63990cd2126f1bebef0f00bb90207b3b8905bd31
Msoto3 <102495266+Msoto3@users.noreply.github.com>
2022-06-15 19:24:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const res = prompt("Please enter the ID number for the pokemon.\nChoose a number from 1-898.\n\nRefresh the page to choose a new Pokemon.");
+
+const apiData = {
+    url: 'https://pokeapi.co/api/v2/',
+    type: 'pokemon',
+    id: res,
+}
+
+const {url,type,id} = apiData;
+const apiUrl = `${url}${type}/${id}`
+
+fetch(apiUrl)
+    .then( (data) => {
+        if(data.ok){
+            return data.json()
+        }
+        throw new Error('Response not ok.'); 
+    })
+    .then((data) => genHtml(data))
+    .catch( error => console.error('Error:', error))
+const genHtml = (data) =>{
+    console.log(data)
+    const html = `
+        <div class='name'>${data.name}</div>
+        <img src=${data.sprites.front_default}>
+        <div class='details'>
+            <span>#${data.id}</span>
+        </div>
+        <div class='details'>${data.types[0].type.name}</div>
+    `
+    const pokDiv = document.querySelector('.pokemon')
+    pokDiv.innerHTML = html
+
+}
